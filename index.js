c94d7fcc4b872321dc4202da8491a1edcf073332
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-08-25 12:01:32 +0400

    create user table with prisma

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
+});
+
+app.post('/', async (req, res) => {
+  const newUser = await prisma.user.create({ data: req.body });
+  res.json(newUser);
+});
 
 app.listen(4000, () => {
   console.log('Server is listening on port 4000');
