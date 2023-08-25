164dee0f8c4083c8d0ce60b958c0710ea4e2ca40
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-08-25 12:37:33 +0400

    create endpoints and crud operacions

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ app.post('/', async (req, res) => {
   res.json(newUser);
 });
 
+app.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const newFirstName = req.body.firstName;
+  const updatedUser = await prisma.user.update({
+    where: { id: parseInt(id) },
+    data: { firstName: newFirstName },
+  });
+  res.json(updatedUser);
+});
+
+app.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  const deletedUser = await prisma.user.delete({
+    where: { id: parseInt(id) },
+  });
+  res.json(deletedUser);
+});
+
 app.listen(4000, () => {
   console.log('Server is listening on port 4000');
 });
