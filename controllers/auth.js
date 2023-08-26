02e968481d08f26c2ed1a1fdb38f592f609a4e52
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-08-26 16:25:24 +0400

    create register, login and logout functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 
@@ -40,6 +41,48 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
 
-export const logout = (req, res) => {};
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found!' });
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid email or password.' });
+    }
+
+    const token = jwt.sign({ id: user.id }, 'secretKey');
+
+    const { password: _, ...userData } = user;
+
+    res
+      .cookie('accessToken', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(userData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while logging in.' });
+  }
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie('accessToken', {
+      secure: true,
+      sameSite: 'none',
+    })
+    .status(200)
+    .json('User has been logged out');
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import commentRoutes from './routes/comments.js';
@@ -10,6 +12,8 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
