d2116729730ec451fb10c3247e0e40a506afaa45
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-09-21 03:15:40 +0400

    remove nodemon

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://client-review-seven.vercel.app',
     methods: 'GET, POST, PUT, DELETE',
     credentials: true,
   })
