92a9002ac95ca3d9369df07838f9145e4f71455e
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-09-21 11:26:23 +0400

    change database url

--- prisma/migrations/20230919195456_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `post` DROP FOREIGN KEY `Post_authorId_fkey`;
-
--- AlterTable
-ALTER TABLE `post` MODIFY `sumRating` VARCHAR(191) NULL DEFAULT '0';
-
--- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
