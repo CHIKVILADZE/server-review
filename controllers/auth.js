7aed14e8a8440a7b043660136ff39488079fdd31
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-09-22 08:28:40 +0400

    some changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
 
     const { password: _, ...others } = user;
 
-    res.status(200).json(token);
+    // res.status(200).json(token);
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
