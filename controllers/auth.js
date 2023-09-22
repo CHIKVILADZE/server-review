df208e56e1fa923c01302e03ef581f119b98ca13
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-09-22 07:16:55 +0400

    some changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,7 +60,7 @@ export const login = async (req, res) => {
       return res.status(401).send('Invalid email or password.');
     }
 
-    const accessToken = jwt.sign({ id: user.id });
+    const accessToken = jwt.sign({ id: user.id }, 'secretKey');
 
     req.session.accessToken = accessToken;
 
