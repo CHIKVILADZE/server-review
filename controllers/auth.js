eb5747870cff878ae104b95a0977cf025e75ec45
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-09-22 07:39:36 +0400

    save accesstoken to localstorage

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,8 +61,12 @@ export const login = async (req, res) => {
     }
 
     const accessToken = jwt.sign({ id: user.id }, 'secretKey');
+
     res.cookie('accessToken', accessToken, { httpOnly: true });
+
     req.session.accessToken = accessToken;
+
+    localStorage.setItem('accessToken', accessToken);
 
     const { password: _, ...others } = user;
 
