1dcb30d7939510e2df0a495487c02bea9f93c7e2
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-09-22 06:13:54 +0400

    take token

--- controllers/movie.js
+++ controllers/movie.js
@@ -40,46 +40,36 @@ export const getMovieById = async (req, res) => {
 };
 
 export const addMovie = async (req, res) => {
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json('Not logged in!');
-
-  jwt.verify(token, 'secretkey', async (err, decodedToken) => {
-    if (err) {
-      console.error('Token verification error:', err);
-      return res.status(403).json('Token is not valid!');
-    }
-
-    try {
-      const newMovie = await prisma.movie.create({
-        data: {
-          name: req.body.name,
-          rating: req.body.rating,
-          text: req.body.text,
-          group: 'movie',
-          author: {
-            connect: {
-              id: req.body.userId,
-              firstName: req.body.firstName,
-              lastName: req.body.lastName,
-              googleId: req.body.googleId,
-              githubId: req.body.githubId,
-            },
-          },
-          post: {
-            connect: {
-              id: req.body.postId,
-            },
+  try {
+    const newMovie = await prisma.movie.create({
+      data: {
+        name: req.body.name,
+        rating: req.body.rating,
+        text: req.body.text,
+        group: 'movie',
+        author: {
+          connect: {
+            id: req.body.userId,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            googleId: req.body.googleId,
+            githubId: req.body.githubId,
           },
         },
-        include: {
-          author: true,
+        post: {
+          connect: {
+            id: req.body.postId,
+          },
         },
-      });
+      },
+      include: {
+        author: true,
+      },
+    });
 
-      return res.status(200).json(newMovie);
-    } catch (error) {
-      console.error('Error creating movie:', error);
-      return res.status(500).json(error);
-    }
-  });
+    return res.status(200).json(newMovie);
+  } catch (error) {
+    console.error('Error creating movie:', error);
+    return res.status(500).json(error);
+  }
 };
